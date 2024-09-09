0bf91344d57bcbbe1b252f7d366abc3341b1fed3
Filszu <filipszumowski05@gmail.com>
2024-09-09 23:16:16 +0200

    Refactor MusicList component and update AdSense strategy

--- components/MusicList.tsx
+++ components/MusicList.tsx
@@ -22,8 +22,10 @@ const MusicList = ({
                         isAdmin={isAdmin}
                     />
                     {(index + 1) % 4 === 0 && (
-                        <section className="w-full flex justify-center flex-col h-auto">
+                        <section className="w-full flex justify-center items-center flex-col h-auto">
                             {/* <p className="text-sm text-center"></p> */}
+
+                            {/* <div className="w-12 bg-slate-300 h-32"></div> */}
                             <AdBanner
                                 dataAdFormat="auto"
                                 dataFullWidthResponsive={true}
--- components/ads/AdSense.tsx
+++ components/ads/AdSense.tsx
@@ -12,7 +12,7 @@ const AdSense = ({ pId }: AdsenseTypes) => {
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
         crossOrigin='anonymous'
-        strategy='lazyOnload'
+        strategy='afterInteractive'
     />
   )
 }
