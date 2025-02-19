d8fbeedcde35599c4bd8f5aab2c3d1eb596084a6
Filszu <filipszumowski05@gmail.com>
2025-02-19 23:04:28 +0100

    fixed spotify regex

--- lib/postSong.ts
+++ lib/postSong.ts
@@ -11,35 +11,30 @@ export default async function postSong(formData: FormData) {
     if (!songUrl) return null;
 
     // Check if the song URL is a valid YouTube or Spotify URL
-
     const youtubeRegex = /^(https?:\/\/)?(music\.youtube\.com\/watch\?v=)\w+/;
-
-    const spotifyRegex =
-        /^(https?:\/\/)?(www\.)?(open\.spotify\.com|spotify\.com\/track)\//;
+    const spotifyTrackRegex = /^(https?:\/\/)?(www\.)?(open\.spotify\.com\/track\/)([\w]+)(\?.*)?$/;
 
     if (!songUrl) {
         console.error('Invalid song URL');
         return null;
     }
-    if (!youtubeRegex.test(songUrl) && !spotifyRegex.test(songUrl)) {
-        if (!youtubeRegex.test(songUrl)) {
-            console.log('Invalid youtube url');
-            return null;
-        }
 
-        if (!spotifyRegex.test(songUrl)) {
-            console.log('Invalid spotify url');
-            return null;
-        }
+    let trimmedSongUrl = songUrl;
+    const spotifyMatch = songUrl.match(spotifyTrackRegex);
+    if (spotifyMatch) {
+        trimmedSongUrl = `https://open.spotify.com/track/${spotifyMatch[4]}`;
+    } else if (!youtubeRegex.test(songUrl)) {
+        console.log('Invalid song URL');
+        return null;
     }
 
-    console.log('Song URL is valid', songUrl);
+    console.log('Song URL is valid', trimmedSongUrl);
 
-    // that means that the song is already in the DB
+    // Check if the song is already in the DB
     let { data: uSongs, error: error1 } = await supabase
         .from('uSongs')
         .select('id, url')
-        .eq('url', songUrl)
+        .eq('url', trimmedSongUrl)
         .limit(1)
         .order('created_at', { ascending: false });
 
@@ -52,7 +47,7 @@ export default async function postSong(formData: FormData) {
     } else {
         const { data, error } = await supabase
             .from('uSongs')
-            .insert({ url: `${songUrl}` })
+            .insert({ url: trimmedSongUrl })
             .select('id')
             .limit(1);
 
